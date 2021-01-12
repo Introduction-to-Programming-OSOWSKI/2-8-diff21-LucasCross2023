f331b7b329df3e6a8a167ec24f4816957d75788c
LucasCross2023 <817750@mystma.org>
2021-01-12 16:47:36 +0000

    diff21 try 3

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def diff21(n):
-    if abs(21-n): 
+    if 21>=n: 
         return abs(21-n)
-    elif abs(n>21):
-        return abs(n*2)
+    elif 21<n:
+        return abs(21-n)*2 
     elif abs(n==21):
         return 0 
 
 
-print(diff21(21))
+print(diff21(31))
